4650c1b1545b4a2ec6d1ffcbf76808dbb6c19824
kala89 <arpadkozma89@gmail.com>
2017-06-28 01:04:59 +0200

    add tests to network module, update list with startIndex

--- source/js/helpers/network.js
+++ source/js/helpers/network.js
@@ -1,14 +1,14 @@
 const googleBookAPIUrl = 'https://www.googleapis.com/books/v1/volumes';
 
 const BookApi = {
-    list: (text) => {
-        return fetch(`${googleBookAPIUrl}?q=${text}&printType=books&maxResults=12`);
+    list: (text, start) => {
+        return fetch(`${googleBookAPIUrl}?q=${text}&printType=books&maxResults=12&orderBy=relevance&projection=full${start ? `&startIndex=${start}` : ''}`);
     },
     get: (id) => {
         return fetch(`${googleBookAPIUrl}/${id}`);
     },
     getCarouselData: () => {
-        return fetch(`${googleBookAPIUrl}?q=javascript&printType=books&maxResults=3&showPreorders=true`);
+        return fetch(`${googleBookAPIUrl}?q=javascript&printType=books&maxResults=4&showPreorders=true&orderBy=relevance`);
     }
 };
 
--- source/js/helpers/network.test.js
+++ source/js/helpers/network.test.js
@@ -8,27 +8,55 @@ describe('BookApi', () => {
         fetch.resetMocks();
     });
 
-    it('should send fetch on right url if \'list\' called', () => {
-        // GIVEN
-        const mockQuery = 'testBook';
+    describe('.list call', () => {
+        it('should send fetch on right url', () => {
+            // GIVEN
+            const mockQuery = 'testBook';
 
-        // WHEN
-        BookApi.list(mockQuery);
+            // WHEN
+            BookApi.list(mockQuery);
 
-        // THEN
-        expect(fetch).toHaveBeenCalledWith(`${googleBookAPIUrl}?q=${mockQuery}&printType=books&maxResults=12`);
-        expect(fetch.mock.calls).toHaveLength(1);
+            // THEN
+            expect(fetch).toHaveBeenCalledWith(`${googleBookAPIUrl}?q=${mockQuery}&printType=books&maxResults=12&orderBy=relevance&projection=full`);
+            expect(fetch.mock.calls).toHaveLength(1);
+        });
+
+        it('should send fetch on right url with startIndex', () => {
+            // GIVEN
+            const mockQuery = 'testBook',
+                startIndex = 12;
+
+            // WHEN
+            BookApi.list(mockQuery, startIndex);
+
+            // THEN
+            expect(fetch).toHaveBeenCalledWith(`${googleBookAPIUrl}?q=${mockQuery}&printType=books&maxResults=12&orderBy=relevance&projection=full&startIndex=${startIndex}`);
+            expect(fetch.mock.calls).toHaveLength(1);
+        });
     });
 
-    it('should send fetch on right url if \'get\' called', () => {
-        // GIVEN
-        const mockId = 'testBookId';
+    describe('.get call', () => {
+        it('should send fetch on right url', () => {
+                // GIVEN
+                const mockId = 'testBookId';
 
-        // WHEN
-        BookApi.get(mockId);
+                // WHEN
+                BookApi.get(mockId);
 
-        // THEN
-        expect(fetch).toHaveBeenCalledWith(`${googleBookAPIUrl}/${mockId}`);
-        expect(fetch.mock.calls).toHaveLength(1);
+                // THEN
+                expect(fetch).toHaveBeenCalledWith(`${googleBookAPIUrl}/${mockId}`);
+                expect(fetch.mock.calls).toHaveLength(1);
+        });
+    });
+
+    describe('.getCarouselData call', () => {
+        it('should send fetch on right url', () => {
+                // WHEN
+                BookApi.getCarouselData();
+
+                // THEN
+                expect(fetch).toHaveBeenCalledWith(`${googleBookAPIUrl}?q=javascript&printType=books&maxResults=4&showPreorders=true&orderBy=relevance`);
+                expect(fetch.mock.calls).toHaveLength(1);
+        });
     });
 });
